53f31ed94b004bc679c1f71a0f18c65fd5038f0d
chatinunk97 <chatinun.hobby@gmail.com>
2023-12-21 18:58:55 +0700

    add def to class/function component

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,14 @@ interface AppProps {
   color?: string;
 }
 
+//Nowadays people don't quite use Class Component
 class App extends React.Component<AppProps> {
   render() {
     return <div>{this.props.color}</div>;
   }
 }
 
+//This is the function component way
 // const App = ({ color }: AppProps) => {
 //   return <h1>{color}</h1>;
 // };
