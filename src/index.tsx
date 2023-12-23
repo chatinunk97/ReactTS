5c628763ca6c210d0e5dd5ab83c851be98abde4d
chatinunk97 <chatinun.hobby@gmail.com>
2023-12-23 23:13:21 +0700

    clear comments

--- src/index.tsx
+++ src/index.tsx
@@ -14,39 +14,3 @@ ReactDOM.render(
   </Provider>,
   document.querySelector("#root")
 );
-
-// interface AppProps {
-//   color?: string;
-// }
-// interface AppState {
-//   counter: number;
-// }
-
-// class App extends React.Component<AppProps, AppState> {
-//   constructor(props: AppProps) {
-//     super(props);
-//     this.state = { counter: 0 };
-//   }
-//   onIncrement = (): void => {
-//     this.setState({ counter: this.state.counter + 1 });
-//   };
-//   onDecrement = (): void => {
-//     this.setState({ counter: this.state.counter - 1 });
-//   };
-//   render() {
-//     return (
-//       <div>
-//         <button onClick={this.onIncrement}>Increment</button>
-//         <button onClick={this.onDecrement}>Decrement</button>
-//         {this.state.counter}
-//       </div>
-//     );
-//   }
-// }
-
-//This is the function component way
-// const App = (props: AppProps): JSX.Element => {
-//   return <h1>{props.color}</h1>;
-// };
-
-// ReactDOM.render(<App color="red" />, document.querySelector("#root"));
